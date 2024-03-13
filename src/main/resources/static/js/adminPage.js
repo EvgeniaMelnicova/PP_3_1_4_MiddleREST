7ba372e35b714f47aa17f813acea4539263583ce
Evgenia <Shikme-ai9@mail.ru>
2024-03-13 10:45:05 +0300

    13.03.24. Таблица всех пользователей вынесена.

--- src/main/resources/static/js/adminPage.js
+++ src/main/resources/static/js/adminPage.js
@@ -1,45 +1,6 @@
 const url = '/api/admin/'
 const dbRoles = [{id: 1, name: "ROLE_ADMIN"}, {id: 2, name: "ROLE_USER"}]
 
-let usersInfo = ''
-const showUsers = (users) => {
-    const container = document.getElementById("tbody-admin")
-    const arr = Array.from(users)
-    arr.forEach(user => {
-        usersInfo += `
-        <tr>
-            <td>${user.id}</td>
-            <td>${user.username}</td>
-            <td>${user.name}</td>
-            <td>${user.lastname}</td>
-            <td>${user.age}</td>
-            <td>${user.email}</td>
-            <td>${user.rolesToString}</td>
-            <td class="text text-white">
-                <a class="btnEdit btn btn-info">Edit</a>
-            </td>
-            <td class="text text-white">
-                <a class="btnDelete btn btn-danger">Delete</a>
-            </td>
-        </tr>
-        `
-    })
-    container.innerHTML = usersInfo
-}
-fetch(url)
-    .then(response => response.json())
-    .then(data => showUsers(data))
-    .catch(error => console.log(error))
-
-const reloadShowUsers = () => {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            usersInfo = ''
-            showUsers(data)
-        })
-}
-
 let userInfo = ''
 const showUser = (user) => {
     const container = document.getElementById("tbody-user-info")
