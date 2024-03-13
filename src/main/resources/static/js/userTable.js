857bd037e6d63324fd5c50adfc22ed2dcea73c39
Evgenia <Shikme-ai9@mail.ru>
2024-03-13 09:36:09 +0300

    13.03.24. Информация о пользователе вынесена в фрагмент

--- src/main/resources/static/js/userTable.js
+++ src/main/resources/static/js/userTable.js
@@ -1,12 +1,3 @@
-const showNavbarInfoUser = (user) => {
-    document.getElementById("top-panel").innerHTML =
-        `<h5>${user.username} with roles: ${user.rolesToString}</h5>`
-}
-fetch("/api/user/")
-    .then(response => response.json())
-    .then(data => showNavbarInfoUser(data))
-    .catch(error => console.log(error))
-
 let userPageInfo = ' '
 const showUserInfo = (user) => {
     const container = document.getElementById("tbody-user")
